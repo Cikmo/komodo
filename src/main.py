7a356da6b1e9586d981799c0851acfe82b170514
Christian <contact@komodo.link>
2024-06-01 11:07:16 +0200

    add a run script

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
+"""
+Main module of the application.
+"""
+
 import asyncio
 import os
 
-if os.name != "nt":
+if "nt" not in os.name:
     import uvloop
+
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-    
-    
+
+
 def main():
+    """The entry point of the application."""
     print("Hello, World!")
