2fb19d935fb4643ac4af25ab27512194d4de9d27
Christian <contact@komodo.link>
2024-06-06 01:36:11 +0200

    undo last, just disable the lint on the line instead

--- src/bot.py
+++ src/bot.py
@@ -171,6 +171,8 @@ async def load_all_cogs(bot: Bot) -> None:
     except* commands.errors.ExtensionFailed as eg:
         for e in eg.exceptions:
             logger.exception(e)
+    except* Exception as e:  # pylint: disable=broad-exception-caught
+        logger.exception("Exception thrown while loading cog.", exc_info=e)
     logger.info("Finished loading cogs.")
 
 
@@ -197,6 +199,8 @@ async def unload_all_cogs(bot: Bot) -> None:
     except* commands.errors.ExtensionFailed as eg:
         for e in eg.exceptions:
             logger.exception(e)
+    except* Exception as e:  # pylint: disable=broad-exception-caught
+        logger.exception("Exception thrown while unloading cog.", exc_info=e)
     logger.info("Finished unloading cogs.")
 
 
