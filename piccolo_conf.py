beb9bfdbc03e6c9915a6a4685af3d3b4edd05fba
Christian <contact@komodo.link>
2024-06-15 11:31:14 +0200

    add an error message if database couldnt be connected to

--- piccolo_conf.py
+++ piccolo_conf.py
@@ -9,15 +9,21 @@ from src.config.settings import get_settings
 
 settings = get_settings()
 
-DB = PostgresEngine(
-    config={
-        "database": settings.database.database,
-        "user": settings.database.user,
-        "password": settings.database.password,
-        "host": settings.database.host,
-        "port": settings.database.port,
-    }
-)
+try:
+    DB = PostgresEngine(
+        config={
+            "database": settings.database.database,
+            "user": settings.database.user,
+            "password": settings.database.password,
+            "host": settings.database.host,
+            "port": settings.database.port,
+        }
+    )
+except ConnectionRefusedError:
+    raise ConnectionRefusedError(
+        "Could not connect to the database. Check that the database is running, "
+        "and that the connection settings are correct."
+    ) from None
 
 
 # A list of paths to piccolo apps
