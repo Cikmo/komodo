910ffaf14e515e02d75d603cef07056ef3f9fe4e
Christian <contact@komodo.link>
2024-06-17 01:51:38 +0200

    comment out some annoying temp code

--- src/tables/user_settings.py
+++ src/tables/user_settings.py
@@ -1,15 +1,15 @@
 """This module contains the User table."""
 
-from piccolo.columns import BigInt, ForeignKey, Text
+from piccolo.columns import BigInt, Text
 from piccolo.table import Table
 
 # from src.tables.pnw import Nation
 
 
-# class UserSettings(Table):
-#     """Stores information about users registered with the bot."""
+class UserSettings(Table):
+    """Stores information about users registered with the bot."""
 
-#     discord_id = BigInt(primary_key=True)
-#     nation = ForeignKey(references=Nation)
+    discord_id = BigInt(primary_key=True)
+    # nation = ForeignKey(references=Nation)
 
-#     pnw_api_key = Text(null=True)
+    pnw_api_key = Text(null=True)
